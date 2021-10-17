cf50a010615bdd8ba9d7e1c7d1bcee79be506685
karson777 <karson777@tutanota.com>
2021-10-17 15:01:03 -0700

    adding autostart script

---
+++ .dwm/autostart.sh
@@ -0,0 +1,6 @@
+picom &
+xmodmap ~/.Xmodmap &
+/home/karson777/scripts/75hz.sh &
+feh --bg-fill ~/Pictures/wallpapers/serial-experiments-lain-room-1514392856.jpg &
+/usr/bin/sxhkd &
+/usr/local/bin/slstatus
