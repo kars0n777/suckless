52354aca38fb1b735d5b62cb923838e95eb51ce8
kars0n777 <karson777@tutanota.com>
2021-10-17 00:25:19 -0700

    Update config.h

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "SourceCodePro-Medium:size=9" };
-static const char dmenufont[]       = "SourceCodePro-Medium:size=9";
+static const char *fonts[]          = { "SourceCodePro-Medium:size=10" };
+static const char dmenufont[]       = "SourceCodePro-Medium:size=10";
 static const char col_gray1[]       = "#282a36";
 static const char col_gray2[]       = "#ffb86c";
 static const char col_gray3[]       = "#ff79c6";
