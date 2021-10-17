2b4310a870951cf0a8aefdbe8d6dff2dee73c211
karson777 <karson777@tutanota.com>
2021-10-17 13:27:27 -0700

    patching dwm

--- dwm-6.2/config.def.h
+++ dwm-6.2/config.def.h
@@ -83,8 +83,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[0]} },
 /*	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },*/
 /*	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },*/
-/*	{ MODKEY|ControlMask,		    XK_comma,  cyclelayout,    {.i = -1 } },*/
-	{ MODKEY|ShiftMask,             XK_f,      cyclelayout,    {.i = +1 } },
+	{ MODKEY|ControlMask,		    XK_comma,  cyclelayout,    {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_period, cyclelayout,    {.i = +1 } },
 /*	{ MODKEY,                       XK_o, 	   setlayout,      {0} },*/
 	{ MODKEY,	                    XK_f,      togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -92,7 +92,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_z,      focusmon,       {.i = -1 } },
 /*	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },*/
 /*	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },*/
-	{ MODKEY,                       XK_x,      tagmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_z,      tagmon,         {.i = +1 } },
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -4 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +4 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
