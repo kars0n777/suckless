27613a8e7e8197faa393a4db52cfb5441c5390cc
karson777 <karson777@tutanota.com>
2021-10-18 21:35:42 -0700

    st patch

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -64,13 +64,13 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "kitty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY|ShiftMask,		        XK_Return, spawn,          {.v = dmenucmd } },
 	{ MODKEY, 	                    XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_BackSpace, togglebar,      {0} },
+	{ MODKEY,                       XK_BackSpace, togglebar,   {0} },
 	{ MODKEY,                       XK_t,      focusstack,     {.i = +1 } },
 	/*{ MODKEY,                     XK_k,      focusstack,     {.i = -1 } },*/
 /*	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },*/
