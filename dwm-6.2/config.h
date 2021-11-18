6b49af57cdd19c28f8ae8ec8fd6eaebfaf4b3a43
karson777 <karson777@tutanota.com>
2021-11-18 11:10:54 -0800

    changes

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -45,7 +45,7 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "",      tile },    /* first entry is default */
-	{ " ",     NULL }, /* no layout function means floating behavior */
+	{ "",     NULL }, /* no layout function means floating behavior */
 	{ "",      monocle },
 	{ NULL,       NULL },
 };
