ca34e9c3cfb11e85bef3f2ebde32e4878aa485fb
karson777 <karson777@tutanota.com>
2021-11-18 10:31:13 -0800

    changes

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -66,7 +66,8 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "150x50", NULL }; 
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL }; 
+/*static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "150x50", NULL };*/ 
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
