19802e36dfc6a54cb132a817d3cfa97525892a13
karson777 <karson777@tutanota.com>
2021-11-18 09:40:22 -0800

    changes

--- dwm-6.2/config.def.h
+++ dwm-6.2/config.def.h
@@ -11,8 +11,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = {"JetBrainsMono-Regular:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"};
-static const char dmenufont[]       = "JetBrainsMono-Regular:size=10"; 
+static const char *fonts[]          = {"JetBrainsMono Nerd Font-Regular:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"};
+static const char dmenufont[]       = "JetBrainsMono Nerd Font-Regular:size=10"; 
 static const char col_gray1[]       = "#282a36";
 static const char col_gray2[]       = "#bd93f9";
 static const char col_gray3[]       = "#ff79c6";
@@ -65,11 +65,14 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char scratchpadname[] = "scratchpad";
+static const char *scratchpadcmd[] = { "alacritty",scratchpadname, "-g", "120x34", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY|ShiftMask,		        XK_space,  spawn,          {.v = dmenucmd } },
 	{ MODKEY, 	                    XK_Return, spawn,          {.v = termcmd } },
+	{ Mod1Mask,                     XK_Return,  togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,                       XK_BackSpace, togglebar,   {0} },
 	{ MODKEY,                       XK_t,      focusstack,     {.i = +1 } },
 	/*{ MODKEY,                     XK_k,      focusstack,     {.i = -1 } },*/
@@ -81,7 +84,7 @@ static Key keys[] = {
 /*	{ MODKEY,                       XK_Tab,    view,           {0} },*/
 	{ MODKEY,                       XK_c,      killclient,     {0} },
 	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY|Mod1Mask,              XK_f,      setlayout,      {.v = &layouts[1]} },
 /*	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },*/
 /*	{ MODKEY|ControlMask,		    XK_comma,  cyclelayout,    {.i = -1 } },*/
 /*	{ MODKEY|ControlMask,           XK_period, cyclelayout,    {.i = +1 } },*/
