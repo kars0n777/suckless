f35d31d057a17b8bbf5f8050aef967012d39232f
karson777 <karson777@tutanota.com>
2022-03-29 07:21:36 -0700

    lol

--- st-0.8.5/config.h
+++ st-0.8.5/config.h
@@ -102,7 +102,7 @@ float alpha = 0.7;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
   /* 8 normal colors */
-  [0] = "#000000", /* black   */
+  [0] = "#282A36", /* black   */
   [1] = "#ff5555", /* red     */
   [2] = "#50fa7b", /* green   */
   [3] = "#f1fa8c", /* yellow  */
@@ -112,7 +112,7 @@ static const char *colorname[] = {
   [7] = "#bbbbbb", /* white   */
 
   /* 8 bright colors */
-  [8]  = "#44475a", /* black   */
+  [8]  = "#6272a4", /* black   */
   [9]  = "#ff5555", /* red     */
   [10] = "#50fa7b", /* green   */
   [11] = "#f1fa8c", /* yellow  */
