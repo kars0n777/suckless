3c0de9012836f60bf3fc5b1ff11ee660a41cb29c
karson777 <karson777@tutanota.com>
2022-06-04 13:42:02 -0700

    lol

--- slock-1.4/config.h
+++ slock-1.4/config.h
@@ -4,8 +4,8 @@ static const char *group = "wheel";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "#000000",     /* after initialization */
-	[INPUT] =  "#000000",   /* during input */
-	[FAILED] = "#ff5555",   /* wrong password */
+	[INPUT] =  "#282a36",   /* during input */
+	[FAILED] = "#ff79c6",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
