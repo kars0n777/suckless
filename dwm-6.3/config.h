b71655e63974f5bf5166d6e218a2b5037ceb58c2
karson777 <karson777@tutanota.com>
2022-07-23 22:15:14 -0700

    lol

--- dwm-6.3/config.h
+++ dwm-6.3/config.h
@@ -63,20 +63,21 @@ static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY|ShiftMask,		        XK_Return, spawn,         {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,		        XK_Return, spawn,          {.v = dmenucmd } },
 	{ MODKEY, 	                    XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_BackSpace, togglebar,   {0} },
 	{ MODKEY,                       XK_t,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_y,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_a,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_d,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_space,  zoom,           {0} },
-/*	{ MODKEY,                       XK_Tab,    view,           {0} },*/
+/*	{ MODKEY, 	                    XK_Tab,    view,           {0} },*/
 	{ MODKEY|ShiftMask, 			XK_c,      killclient,     {0} },
-	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY|Mod1Mask,              XK_f,      setlayout,      {.v = &layouts[1]} },
+/*	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[0]} },*/
+/*	{ MODKEY|Mod1Mask,              XK_f,      setlayout,      {.v = &layouts[1]} },*/
 /*	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },*/
-/*	{ MODKEY,                       XK_o, 	   setlayout,      {0} },*/
-/*	{ MODKEY,	                    XK_f,      togglefloating, {0} },*/
+	{ MODKEY|ShiftMask,             XK_f, 	   setlayout,      {0} },
+/*	{ MODKEY|ShiftMask,	            XK_f,      togglefloating, {0} },*/
 	{ MODKEY,                       XK_s,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_s,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_z,      focusmon,       {.i = -1 } },
