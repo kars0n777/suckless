94c8bb5aa938d0d23e2e7b6812a708840bbde883
karson777 <karson777@tutanota.com>
2022-08-05 19:29:28 -0700

    lol

--- dwm-6.3/config.def.h
+++ dwm-6.3/config.def.h
@@ -66,12 +66,14 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *sxhkdcmd[]  = { "sxhkd-toggle.sh", NULL };
 
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY|ShiftMask,		        XK_Return, spawn,          {.v = dmenucmd } },
 	{ MODKEY, 	                    XK_Return, spawn,          {.v = termcmd } },
+	{ ControlMask|Mod1Mask,         XK_s,      spawn,          {.v = sxhkdcmd} },
 	{ MODKEY,                       XK_BackSpace, togglebar,   {0} },
 	{ MODKEY,                       XK_t,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_y,      focusstack,     {.i = -1 } },
