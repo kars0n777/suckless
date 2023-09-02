75fc7494c77400a132c8dc9534e467c6829aa43e
karson777 <karson777@tutanota.com>
2023-09-02 00:39:49 -0700

    lol

--- dwm-6.3/config.h
+++ dwm-6.3/config.h
@@ -2,9 +2,9 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 0;       /* gaps between windows */
+static const unsigned int gappx     = 20;       /* gaps between windows */
 static const unsigned int snap      = 0;        /* snap pixel */
-static const int showbar            = 0;        /* 0 means no bar */
+static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {"mononoki Nerd Font-Regular:size=10", "Apple Color Emoji:pixelsize=16:antialias=true:autohint=true"};
 static const char dmenufont[]       = "mononoki Nerd Font-Regular:size=10"; 
